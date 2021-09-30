02fc6a76caaae5635f5158faea5d77b7617983e3
oritsegbubemi <86294092+gbubemi22@users.noreply.github.com>
2021-09-30 14:47:56 +0100

    second commit

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const dotenv = require('dotenv');
 const EnglishClubRoutes = require('./routes/englishPeremierleague');
 const leagueRoutes = require('./routes/league');
 const ArsenalPlayersRoutes = require('./routes/arsenalPlayers');
+const ChelseaRoutes = require('./routes/chelsea');
+const ManchesterRoutes = require('./routes/manchesterUtd');
+const LiverpoolRoutes = require('./routes/liverpoolfc');
+const MancityRoutes = require("./routes/manCityFc");
+
 
 
 dotenv.config();
@@ -28,6 +33,10 @@ mongoose.connect(process.env.MONGO_DB
      app.use('/api/premierleague', EnglishClubRoutes);
      app.use('/api/leagues', leagueRoutes);
      app.use('/api/arsenalplayer',ArsenalPlayersRoutes);
+     app.use('/api/chelseaplayer', ChelseaRoutes);
+     app.use('/api/manchesterutdplayer', ManchesterRoutes);
+     app.use('/api/liverpoolplayer', LiverpoolRoutes);
+     app.use('/api/mancityplayer', MancityRoutes)
 
      
 
